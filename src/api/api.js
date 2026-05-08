ef614f01a8e6eb051cc8c090df2d547eae24bcd5
Jayarathinavel <Jayrathinavel@gmail.com>
2026-05-08 20:30:31 +0530

    Environment Variables

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // NestJS backend
+  baseURL: import.meta.env.VITE_API_URL, // NestJS backend
 });
 
 // Store navigate function globally (will be set by the hook)
